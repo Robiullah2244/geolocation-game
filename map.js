61530d252428be3751d7daacb2c8d3a1dc2c0ff9
Robiullah2244 <robiullah2244@gmail.com>
2022-08-26 16:40:58 +0200

    center and zoom fixed

--- map.js
+++ map.js
@@ -13,12 +13,12 @@ var region = region || urlParams.get('region');
 function initMap() {
     var yourLat = 56.211379240824726, yourLng = -117.77755895147486;
     if(region == 'NorthAmerica') {yourLat = 66.211379240824726; yourLng = -105.77755895147486}
-    else if(region == 'Europe') {yourLat = 63.772076393350844; yourLng = 16.851560837631457}
-    else if(region == 'Africa') {yourLat = -3.195734213534522; yourLng = 14.721297653247087}
+    else if(region == 'Europe') {yourLat = 56.772076393350844; yourLng = 16.851560837631457}
+    else if(region == 'Africa') {yourLat = -0; yourLng = 14.721297653247087}
      
     // Map options
     var mapOptions = {
-        zoom: 3,
+        zoom: region == 'NorthAmerica' ? 2.7 : region == 'Europe' ? 3.8 : 3.6,
         center: { lat: yourLat, lng: yourLng },
         mapTypeId: google.maps.MapTypeId.ROADMAP,
         backgroundColor: '#FFF',
